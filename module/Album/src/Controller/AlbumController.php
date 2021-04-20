7bc978852cab65f882444f123e73cbbf53f33625
Eric Diedrichs <eric.diedrichs@motionlab.co.uk>
2021-04-20 10:27:03 +0100

    commit.10 ListingAlbums

--- module/Album/src/Controller/AlbumController.php
+++ module/Album/src/Controller/AlbumController.php
@@ -13,9 +13,11 @@ class AlbumController extends AbstractActionController {
     public function __construct(AlbumTable $table) {
         $this->table = $table;
     }
-    
-    public function indexAction() {
 
+    public function indexAction() {
+        return new ViewModel([
+            'albums' => $this->table->fetchAll(),
+        ]);
     }
 
     public function addAction() {
