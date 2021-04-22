84f2356d77b46fc62af48f9cf0e8966747ad6fb1
Eric Diedrichs <eric.diedrichs@motionlab.co.uk>
2021-04-22 14:44:06 +0100

    commit.23 Paginator

--- module/Album/src/Model/AlbumTable.php
+++ module/Album/src/Model/AlbumTable.php
@@ -3,7 +3,11 @@
 namespace Album\Model;
 
 use RuntimeException;
+use Laminas\Db\ResultSet\ResultSet;
+use Laminas\Db\Sql\Select;
 use Laminas\Db\TableGateway\TableGatewayInterface;
+use Laminas\Paginator\Adapter\DbSelect;
+use Laminas\Paginator\Paginator;
 
 class AlbumTable {
     private $tableGateway;
@@ -12,9 +16,39 @@ class AlbumTable {
         $this->tableGateway = $tableGateway;
     }
 
-    public function fetchAll() {
+    public function fetchAll($paginated = false) {
+        if ($paginated) {
+            return $this->fetchPaginatedResults();
+        }
+
         return $this->tableGateway->select();
     }
+
+    private function fetchPaginatedResults() {
+        // Create a new Select object for the table:
+        $select = new Select($this->tableGateway->getTable());
+
+        // Create a new result set based on the Album entity:
+        $resultSetPrototype = new ResultSet();
+        $resultSetPrototype->setArrayObjectPrototype(new Album());
+
+        // Create a new pagination adapter object:
+        $paginatorAdapter = new DbSelect(
+            // our configured select object:
+            $select,
+            // the adapter to run it against:
+            $this->tableGateway->getAdapter(),
+            // the result set to hydrate:
+            $resultSetPrototype
+        );
+
+        $paginator = new Paginator($paginatorAdapter);
+        return $paginator;
+    }
+
+    // public function fetchAll() {
+    //     return $this->tableGateway->select();
+    // }
 
     public function getAlbum($id) {
         $id = (int) $id;
